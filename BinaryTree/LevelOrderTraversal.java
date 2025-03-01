e448d8dd22a6c019b1d7b92d6fea48ec2921e385
KeerthiErnapu <127508885+KeerthiErnapu@users.noreply.github.com>
2025-03-01 23:35:32 +0530

    Create LevelOrderTraversal.java

---
+++ BinaryTree/LevelOrderTraversal.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size(); // Number of elements in the current level
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+}
