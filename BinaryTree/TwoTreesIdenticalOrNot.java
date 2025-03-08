e0b7ba21c597b4dc1a458f077876d0eaac7592ae
KeerthiErnapu <127508885+KeerthiErnapu@users.noreply.github.com>
2025-03-08 22:45:18 +0530

    Create TwoTreesIdenticalOrNot.java

---
+++ BinaryTree/TwoTreesIdenticalOrNot.java
@@ -0,0 +1,12 @@
+class BinaryTree {
+    public static boolean areIdentical(Node root1, Node root2) {
+        if (root1 == null && root2 == null) 
+            return true;
+        if (root1 == null || root2 == null) 
+            return false;
+
+        return (root1.data == root2.data) &&
+               areIdentical(root1.left, root2.left) &&
+               areIdentical(root1.right, root2.right);
+    }
+}
