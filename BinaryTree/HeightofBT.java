fc76f5a9aed24228f25b124e43e00bc22cf78c7b
KeerthiErnapu <127508885+KeerthiErnapu@users.noreply.github.com>
2025-03-08 22:05:43 +0530

    Update HeightofBT.java

--- BinaryTree/HeightofBT.java
+++ BinaryTree/HeightofBT.java
@@ -1,9 +1,23 @@
-#Approach 1
+#height based on no.of nodes
 class Solution {
     // Function to find the height of a binary tree.
     int height(Node node) {
         if (node == null)
             return 0;
+        
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+# height based on no.of edges
+class Solution {
+    // Function to find the height of a binary tree.
+    int height(Node node) {
+        if (node == null)
+            return -1;
         
         int leftHeight = height(node.left);
         int rightHeight = height(node.right);
